19276290407212dc9f788a34984d3284fd98e910
Armand-Cezar Mathe <me@cezarmathe.com>
2021-06-04 08:40:13 +0300

    Add binary for stevebot

---
+++ cmd/stevebot/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/cezarmathe/stevebot/internal/bot"
+	"github.com/cezarmathe/stevebot/internal/steve"
+	"go.uber.org/zap"
+)
+
+const (
+	// fixme 23/05/2021: add configurable graceful shutdown timeout
+	//
+	// This timeout is sensible to the time it takes for discord to gracefully
+	// disconnect.
+	gracefulShutdownTimeout = time.Second * 2
+)
+
+var (
+	// Version is the build version of stevebot.
+	Version string
+)
+
+var (
+	log *zap.SugaredLogger
+)
+
+func init() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to initialize logger")
+	}
+	log = logger.Sugar().Named("main")
+}
+
+func main() {
+	log.Infow("hello, this is stevebot", "version", Version)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := new(sync.WaitGroup)
+
+	err := steve.NewSteve(ctx, wg)
+	if err != nil {
+		log.Errorw("failed to create a new steve", "err", err)
+		cancel()
+		shutdown(wg)
+	}
+	err = bot.NewBot(ctx, wg)
+	if err != nil {
+		log.Errorw("failed to create a new bot", "err", err)
+		cancel()
+		shutdown(wg)
+	}
+
+	err = steve.Get().Start()
+	if err != nil {
+		log.Errorw("failed to start steve", "err", err)
+		cancel()
+		shutdown(wg)
+	}
+	err = bot.Get().Start()
+	if err != nil {
+		log.Errorw("failed to start bot", "err", err)
+		cancel()
+		shutdown(wg)
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT)
+	<-sig
+	cancel()
+
+	shutdown(wg)
+}
+
+func shutdown(wg *sync.WaitGroup) {
+	done := make(chan byte, 1)
+	go func() {
+		wg.Wait()
+		done <- 0x0
+	}()
+
+	timeout, cancelTimeout := context.WithTimeout(
+		context.Background(),
+		gracefulShutdownTimeout)
+
+	select {
+	case <-done:
+		cancelTimeout()
+		log.Info("have a nice day")
+		os.Exit(0)
+	case <-timeout.Done():
+		cancelTimeout()
+		log.Errorw("graceful shutdown timed out after, shutting down now",
+			"timeout",
+			gracefulShutdownTimeout.String())
+		os.Exit(1)
+	}
+}
